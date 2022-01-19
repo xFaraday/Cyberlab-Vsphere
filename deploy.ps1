4da0ac11c8f13a1b70ea5b4a88051c1b2e4b6488
xfaraday <theonlyfaraday@gmail.com>
2022-01-19 15:10:36 -0800

    updated scripts

--- deploy.ps1
+++ deploy.ps1
@@ -1,33 +1,34 @@
 #Deploy script P O W E R CLI
 #inputs:
 #user array, deployment name, VMs that are not supposed to be shown, cluster host, datastore
-$serv = Connect-VIServer -Server 
- 
+$credentials = Get-VICredentialStoreItem -Host cyberlab.csusb.edu -File C:\Users\Administrator\Documents\Cyberlab-vsphere-master\creds.xml
+$serv = Connect-VIServer cyberlab.csusb.edu -User $credentials.User -Password $credentials.Password
+
 #userlist
 $users=get-content ""
- 
-#folder name
+
+#folder name to create
 $masterdeploymentname= ""
- 
+
 #VMs to clone, TEMPLATES
 $srcvms = get-vm -location ""
- 
+
 #resources
-$datastore = Get-Datastore RS2-Prod3-DS
-$vmhost = get-vmhost 
- 
+$datastore = Get-Datastore Production
+$vmhost = get-vmhost host17.cyberlab.csusb.edu
+
 #Portgroup to clone
 $portgroup = ""
 $portgroupsrc = Get-VDPortgroup -Name $portgroup
 $portgroupsrcname = (Get-VDPortgroup -Name $portgroup).Name
- 
+
 #Student role for permissions
 $studentrole = Get-VIRole -name Student
 $prod = Get-Folder -Server $serv -Name 03-Production
- 
+
 #$masterdeploymentfolder=Get-Folder -Server $serv -Name GenCyber -Location 03-Production
 $masterdeploymentfolder = New-Folder -Name $masterdeploymentname -Location $prod
- 
+
 function generatenewvlanID() {
     $ran = Get-Random -Maximum 3500
     $vlan = (Get-VDPortGroup -VDSwitch "Pods").VlanConfiguration
@@ -37,7 +38,7 @@ function generatenewvlanID() {
         return $ran
     }
 }
- 
+
 foreach ($u in $users) {
     $createdfolder = New-Folder -Name $u -Location $masterdeploymentfolder
     #clone new vms
@@ -60,7 +61,7 @@ foreach ($u in $users) {
     $newvms = get-vm -location $createdfolder
     Foreach ($newvm in $newvms){
         $newvm | get-networkadapter | where {$_.NetworkName -eq $portgroup} | Set-NetworkAdapter -NetworkName $createdportgroup -confirm:$false
-        if(($newvm).Name -eq "Kali") {
+        if(($newvm).Name -eq "Kali 2.0") {
             New-VIPermission -Role $studentrole -Principal "CSUSB\$u" -Entity $newvm
         }
     }
--- undeploy.ps1
+++ undeploy.ps1
@@ -1,21 +1,44 @@
 # P O W E R C L I 
 # mass undeploy vm script
- 
-$serv = Connect-VIServer -Server 
- 
+$credentials = Get-VICredentialStoreItem -Host cyberlab.csusb.edu -File C:\Users\Administrator\Documents\Cyberlab-vsphere-master\creds.xml
+$serv = Connect-VIServer cyberlab.csusb.edu -User $credentials.User -Password $credentials.Password
+
 #vm folder to destroy
 $masterdeploymentname=""
- 
+
 $vms = get-vm -Location $masterdeploymentname
 $networklist=(Get-VM -Location $masterdeploymentname | get-networkadapter).NetworkName | select -Unique
- 
+
+function jobcheck() {
+    $jobs = Get-Job
+    foreach($job in $jobs) {
+        if (($job).State -eq "Running") {
+            return $false
+        }  
+        else {
+            return $true
+        }
+    }
+}
+
 foreach ($vm in $vms) {
     if (($vm).PowerState -eq "PoweredOn") {
         stop-vm -VM $vm -Confirm:$false 
     }
-    Remove-VM -VM $vm -DeletePermanently -RunAsync -confirm:$false
+    #Start-Job -ScriptBlock {
+        
+    Remove-VM -VM $vm -Server $serv -DeletePermanently -RunAsync -confirm:$false
+    #}
 }
- 
+
+#Do {
+#    Start-Sleep -s 5
+#} Until(jobcheck)
+
+start-sleep -s 45
+
 foreach ($portgroup in $networklist) {
-    Get-VirtualPortgroup | where {($_.Name -eq $portgroup) -and ($_.Name -ne "DH-NAT-USERS")} | Remove-VirtualPortGroup -confirm:$false
+    Get-VDPortGroup | where {($_.Name -eq $portgroup) -and ($_.Name -ne "DH-NAT-USERS")} | Remove-VDPortgroup -RunAsync -confirm:$false
 }
+
+Remove-Folder -Folder $masterdeploymentname -confirm:$false
